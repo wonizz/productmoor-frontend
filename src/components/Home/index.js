e2bda534a532d3b22981d79798ad52be1ab9acc1
wonizz <growuplove7@naver.com>
2020-04-01 14:35:15 +0900

    [FRONT] infinite scroll 기능 추가

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -21,7 +21,8 @@ const mapStateToProps = state => ({
   ...state.article,
   appName: state.common.appName,
   token: state.common.token,
-  category: state.common.category
+  category: state.common.category,
+  from: state.articleList.from
 });
 
 
@@ -44,13 +45,31 @@ const masonryOptions = {
   transitionDuration: 0
 };
 const imagesLoadedOptions = { background: '.my-bg-image-el' }
+
 class Home extends React.Component {
   componentWillMount() {
     const tab = this.props.token ? 'feed' : 'all';
     const articlesPromise = this.props.token ?
       agent.Articles.feed :
       agent.Articles.all;
+    let number = 10;
+    const handleScroll = () => {
+      const { innerHeight } = window;
+      const { scrollHeight } = document.body;
+
+      // IE에서는 document.documentElement 를 사용.
+      const scrollTop =
+        (document.documentElement && document.documentElement.scrollTop) ||
+        document.body.scrollTop;
+      // 스크롤링 했을때, 브라우저의 가장 밑에서 100정도 높이가 남았을때에 실행하기위함.
+      if (scrollHeight - innerHeight - scrollTop < 100) {
+        window.clickBlock = true;
+        number += 10;
+        this.props.onLoadMore(agent.Articles.onLoadMore(2, number))
+      }
+    };
     this.props.onLoad(tab, articlesPromise, Promise.all([articlesPromise()]));
+    window.addEventListener('scroll', handleScroll, true);
   }
 
   componentWillUnmount() {
@@ -85,13 +104,13 @@ class Home extends React.Component {
               onClickDetail={this.props.onDetailLoad}
               onLoadMore={this.props.onLoadMore}
             />
-            <Tags 
+            <Tags
               onClickDetail={this.props.onDetailLoad}
-              onDetailUnLoad={this.props.onDetailUnLoad} 
+              onDetailUnLoad={this.props.onDetailUnLoad}
               onRelatedLoadMore={this.props.onRelatedLoadMore}
-              detail={this.props.detail} 
-              online={this.props.online} 
-              related={this.props.related} 
+              detail={this.props.detail}
+              online={this.props.online}
+              related={this.props.related}
               relatedFrom={this.props.from}
             />
             {/* <div classNameName="col-md-3">
