8ce2d4e63676f3d8375127a9ecc0082126d832c9
wonizz <growuplove7@naver.com>
2020-04-02 19:27:42 +0900

    [FRONT] API 호출 로직 수정

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,43 +1,101 @@
-import ArticlePreview from './ArticlePreview';
-import ListPagination from './ListPagination';
-import React from 'react';
+import {
+  ARTICLE_FAVORITED,
+  ARTICLE_UNFAVORITED,
+  SET_PAGE,
+  APPLY_TAG_FILTER,
+  HOME_PAGE_LOADED,
+  HOME_PAGE_UNLOADED,
+  CHANGE_TAB,
+  PROFILE_PAGE_LOADED,
+  PROFILE_PAGE_UNLOADED,
+  PROFILE_FAVORITES_PAGE_LOADED,
+  PROFILE_FAVORITES_PAGE_UNLOADED,
+  HOME_PAGE_LOADED_MORE
+} from '../constants/actionTypes';
 
-const ArticleList = props => {
-  if (!props.articles) {
-    return (
-      <div className="article-preview">Loading...</div>
-    );
+export default (state = {}, action) => {
+  switch (action.type) {
+    case ARTICLE_FAVORITED:
+    case ARTICLE_UNFAVORITED:
+      return {
+        ...state,
+        articles: state.articles.map(article => {
+          if (article.slug === action.payload.article.slug) {
+            return {
+              ...article,
+              favorited: action.payload.article.favorited,
+              favoritesCount: action.payload.article.favoritesCount
+            };
+          }
+          return article;
+        })
+      };
+    case SET_PAGE:
+      return {
+        ...state,
+        articles: action.payload.articles,
+        articlesCount: action.payload.articlesCount,
+        currentPage: action.page
+      };
+    case APPLY_TAG_FILTER:
+      return {
+        ...state,
+        pager: action.pager,
+        articles: action.payload.hits.hits[0]._source,
+        //articlesCount: action.payload.articlesCount,
+        tab: null,
+        //tag: action.tag,
+        currentPage: 0
+      };
+    case HOME_PAGE_LOADED:
+      return {
+        ...state,
+        pager: action.pager,
+        detail: '',
+        articles: action.payload[0] ? action.payload[0].hits.hits : action.payload.hits.hits,
+        //articlesCount: action.payload[1].articlesCount,
+        currentPage: 0,
+        tab: action.tab,
+        size: 10,
+        from: 0,
+      };
+    case HOME_PAGE_LOADED_MORE:
+      return {
+        ...state,
+        pager: action.pager,
+        detail: '',
+        articles: action.payload[0] ? state.articles.concat(action.payload[0].hits.hits) : state.articles.concat(action.payload.hits.hits),
+        //articlesCount: action.payload[1].articlesCount,
+        currentPage: 0,
+        tab: action.tab,
+        size: 10,
+        from: state.from + 10,
+      };
+    case HOME_PAGE_UNLOADED:
+      return {};
+    case CHANGE_TAB:
+      return {
+        ...state,
+        pager: action.pager,
+        articles: action.payload.articles,
+        articlesCount: action.payload.articlesCount,
+        tab: action.tab,
+        currentPage: 0,
+        tag: null
+      };
+    case PROFILE_PAGE_LOADED:
+    case PROFILE_FAVORITES_PAGE_LOADED:
+      return {
+        ...state,
+        pager: action.pager,
+        articles: action.payload[1].articles,
+        articlesCount: action.payload[1].articlesCount,
+        currentPage: 0
+      };
+    case PROFILE_PAGE_UNLOADED:
+    case PROFILE_FAVORITES_PAGE_UNLOADED:
+      return {};
+    default:
+      return state;
   }
-
-  if (props.articles.length === 0) {
-    return (
-      <div className="article-preview">
-        No articles are here... yet.
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <section className="section">
-        <div className="container">
-          <div className="row masonry-container">
-            {
-              props.articles.map(article => {
-                return (
-                        <ArticlePreview article={article} key={article.slug} />
-                );
-              })
-            }
-          </div>
-        </div>
-        <ListPagination
-        pager={props.pager}
-        articlesCount={props.articlesCount}
-        currentPage={props.currentPage} />
-      </section>
-    </div>
-  );
 };
-
-export default ArticleList;
