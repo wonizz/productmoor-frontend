5af75a39613e2bad5e08c111e3031e6e618db20e
wonizz <growuplove7@naver.com>
2020-04-02 19:25:23 +0900

    [FRONT] API 호출 로직 수정

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import agent from '../../agent';
 
-const Banner = ({category, appName, onClickCategory, onDetailUnLoad }) => {
-  
+const Banner = ({ category, appName, onClickCategory, onDetailUnLoad }) => {
+
   if (!category) {
     return null;
   }
   return (
     <div className="menu-swiper swiper-container">
-    <nav className="menu-moor swiper=wrapper">
-    {
-        category.category.map(category => {
-        const handleClick = ev => {
-          ev.preventDefault();
-          onClickCategory('','',agent.Articles.byCategory(category));
-          onDetailUnLoad();
-        }; 
-        return (
-            <a 
-               href="#" 
-               className="btn-nav swiper-slide"
-               onClick={handleClick}
-            >{category}
-            </a>
-        )
-      })
-    }
-    </nav>
-    <div className="swiper-button-prev"></div>
-    <div className="swiper-button-next"></div>
+      <nav className="menu-moor swiper=wrapper">
+        {
+          category.category.map(category => {
+            const handleClick = ev => {
+              ev.preventDefault();
+              onClickCategory('', '', agent.Articles.byCategory(category));
+              onDetailUnLoad();
+              window.category = category;
+              window.number = 10;
+            };
+            return (
+              <a
+                href="#"
+                className="btn-nav swiper-slide"
+                onClick={handleClick}
+              >{category}
+              </a>
+            )
+          })
+        }
+      </nav>
+      <div className="swiper-button-prev"></div>
+      <div className="swiper-button-next"></div>
     </div>
   );
 };
